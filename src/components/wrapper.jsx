5afe1de097a42d00f65eee3c0d6cadbd609b217e
jamel <jamelnafaa46@gmail.com>
2020-11-04 22:03:20 +0100

    f c

---
+++ src/components/wrapper.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {ListGroup} from 'react-bootstrap'
+
+const Wrapper = ()=>{
+return (
+<ListGroup>
+    <ListGroup.Item>exemple </ListGroup.Item>
+</ListGroup>
+
+
+)
+
+
+}
+
+export default Wrapper
