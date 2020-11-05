fbabdd97ea44f3ffbcdaca3ccc47c748fb927378
jamel nafaa <jamelnafaa46@gmail.com>
2020-11-05 20:03:49 +0100

    remove useless

--- src/App.js
+++ src/App.js
@@ -6,12 +6,10 @@ import { Container, Row, Col } from 'react-bootstrap';
 function App() {
   return (
 
-<Container>
+    <Container>
       <Row className='justify-content-center'>
         <Col lg={6} xs={12}>
-        <br/><br/>
-          <h1 >Todo redux</h1><br/><br/><br/>
-          <Wrapper  />
+          <Wrapper />
         </Col>
       </Row>
     </Container>
